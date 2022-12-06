49e8f3d61474597d702f08e605c9730f35443eb2
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-12-06 13:38:30 +0300

    app render components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { handleInitialData } from "./redux/shared";
+import PrivateRoute from "./utils/private-routes";
+import LoadingBar from "react-redux-loading";
+import Home from "./page/home/home.component";
+import Header from "./components/header/header.component";
+import Polls from "./components/poll-list/poll-list.component";
+import PollPage from "./page/poll/poll-page.component";
+import NewPoll from "./components/new-poll/new-poll.component";
+import LeaderPage from "./page/leaderboard/leaderboard.component";
+import RegisterPage from "./page/auth/register.component";
+import LoginPage from "./page/auth/login.component";
+import ErrorPage from "./page/error/error.component";
+
+import "./App.css";
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <LoadingBar />
+        <Header />
+        <main className="main__body">
+          {!this.props.loading && (
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <PrivateRoute path="/polls" component={Polls} />
+              <PrivateRoute path="/poll/:id" component={PollPage} />
+              <Route path="/add" component={NewPoll} />
+              <PrivateRoute path="/leaderboard" component={LeaderPage} />
+              <Route path="/register" component={RegisterPage} />
+              <Route path="/login" component={LoginPage} />
+              <Route component={ErrorPage} />
+            </Switch>
+          )}
+        </main>
+      </Fragment>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps({ authedUser, polls, users }) {
+  return {
+    loading: authedUser === null && polls == null && users == null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
